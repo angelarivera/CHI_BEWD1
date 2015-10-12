ccedfdf150c871563841c205510c8d595cb539a2
Kent Green <kent.green@gmail.com>
2015-10-12 18:38:09 -0500

    adding my local file for W3L2

--- Week3/Lesson2/Examples/kent_green/kg.mixins.rb
+++ Week3/Lesson2/Examples/kent_green/kg.mixins.rb
@@ -1,17 +1,18 @@
 module Upvotable
-  def upvote!
+  def upvote! #this is one method of the module
     @upvote += 1
   end
 
-  def downvote!
+  def downvote! #this is another method of the module. functionality goes here. 
     @upvote -= 1
   end
 end
 
+#if module wasn't in the same file, you'd have to require it w/ require_relative
 class Photo
   attr_reader :photographer, :resolution, :upvotes
 
-  include Upvotable
+  include Upvotable #this tells you to get the Module you want
 
   def initialize(photographer, resolution)
     @photographer = photographer
@@ -23,7 +24,7 @@ end
 class Story
   attr_reader :title, :author, :upvotes
 
-  include Upvotable
+  include Upvotable #again, this tells you to get the Module you want
 
   def initialize(title, author)
     @title = title
