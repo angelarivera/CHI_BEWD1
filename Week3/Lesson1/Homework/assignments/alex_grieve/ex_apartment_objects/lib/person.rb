8fc936dacda9fadaf450c85ca57bf8857fb7eb9b
Alex <grievea86@gmail.com>
2015-10-12 16:04:06 -0500

    Apartment and Secret Number Homework.

--- Week3/Lesson1/Homework/assignments/alex_grieve/ex_apartment_objects/lib/person.rb
+++ Week3/Lesson1/Homework/assignments/alex_grieve/ex_apartment_objects/lib/person.rb
@@ -1,4 +1,17 @@
-#Person Class.
+# Create a person class
+# For this assignment we need to simply hold data
+# No methods are necessary
 class Person
+	attr_accessor :name, :credit_score, :gender
 
+	def initialize(name, credit_score, gender)
+		@name = name
+		@credit_score = credit_score
+		@gender = gender
+	end
+
+	# Gracefully return the name if asked
+	def to_s
+		"#{self.name}"
+	end
 end
--- Week3/Lesson1/Homework/assignments/alex_grieve/ex_secret_number_objects/lib/person.rb
+++ Week3/Lesson1/Homework/assignments/alex_grieve/ex_secret_number_objects/lib/person.rb
@@ -1,3 +1,19 @@
+# Person class contains the following methods:
+#   Initialize: 
+#       - Initialize and set defaults 
+#   to_s: 
+#       - print the player's the name in a graceful way
+
 class Person
-# this class will likely be simple, and just remember its name
+	attr_accessor :name
+
+	# Receive the name from the user
+	def initialize(name)
+		@name = name
+	end
+
+	# Gracefully return the name if asked
+	def to_s
+		"#{self.name}"
+	end
 end
