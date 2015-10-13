68cd20736d075b20a9a15b909dc5a5ec34e6de62
Matt Carrington <coffeeslug@gmail.com>
2015-10-13 10:43:48 -0500

    Added Week3/Lesson1 hmework files and Week3/Lesson2 ex files

---
+++ Week3/Lesson1/Homework/m_carrington/secret_number_objects/lib/game.rb
@@ -0,0 +1,51 @@
+require 'lib/person'
+require 'lib/secret_number'
+# Game
+#	This class holds most of the game logic and should:
+#		Welcome players and inform them of the game rules.
+#		Initialize the Player class.
+# 		Initialize the Secret Number class.
+# 		Prompt the user to choose a number, verify if the user guessed correctly.
+# 		If the user guesses incorrectly let them know if they were too high or too low.
+#		Monitor how many guesses the player has before the game is over.
+#
+# In creating your game, make sure it has arrays, hashes and all functionality is wrapped in methods.
+# Add helpful comments to your code to document what each section does.
+#
+# Tips: Copy paste your code from homework 1 where necessary.
+class Game
+# this class will be complex
+# we need to write logic to initialize a new game, and run the game loop
+# we'll want to write a few separate methods for unique tasks so that our
+# code is structured properly
+	attr_accessor :player, :secret_number, :guess, :guesses_left
+
+	def initialize (player, secret_number, guess, guesses_left)
+		@player = Person.new.player_name
+		@secret_number = SecretNumber.new.random_num
+		@guess = guess
+		@guesses_left = guesses_left
+	end
+
+	def number_check
+		@guesses_left = 3
+		until @guesses_left > 0 do
+		 	@guess = gets.strip.to_i
+			if @guess > @secret_number
+				@guesses_left -= 1
+				puts "That's too high! Only #{guesses_left.to_s} more guesses!"
+			elsif @guess < @secret_number
+				@guesses_left -= 1
+				puts "That's too low! Only #{guesses_left.to_s} more guesses!"
+			elsif @guess == @secret_number
+				puts "You guessed the number! Great job! Have fun being an human for the rest of your pitiful life."
+				exit
+			else
+				puts "You lose. The answer was #{@secret_number.to_s}. Please step into the machine on your left."
+				exit
+			end
+			
+		end
+	end
+
+end
